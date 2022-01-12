932bd4c8ff0dca1b3fd010a6570264988d1e601f
Eve Kozina <eve.kozina@gmail.com>
2022-01-12 16:20:27 -0500

    commit tree d6d60168e15d52725567fe8211532e6200b348b1

---
+++ app/serializers/author_posts_serializer.rb
@@ -0,0 +1,10 @@
+class AuthorPostsSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+
+  belongs_to :author
+  has_many :tags, serializer: TagSerializer
+end
